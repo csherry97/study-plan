3ff99e69667a9f12cc35de19d2ae72510a6cdcf5
csherry97 <zycai0227@gmail.com>
2020-06-12 14:28:24 +0800

    fix popup form close

--- miniprogram/pages/todo/todo.js
+++ miniprogram/pages/todo/todo.js
@@ -36,8 +36,63 @@ Page({
     this.setData({target_minute: e.detail.value});
    },
 
+   /**弹窗动画**/
+   powerDrawer: function (e) {  
+    var currentStatu = e.currentTarget.dataset.statu;  
+    this.util(currentStatu)  
+  },  
+
+  util: function(currentStatu){  
+    /* 动画部分 */  
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({  
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    }); 
+
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation;
+
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step();  
+  
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({  
+      animationData: animation.export()  
+    })  
+      
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {  
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step();  
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({  
+        animationData: animation  
+      })  
+        
+      //关闭  
+      if (currentStatu == "close") {  
+        this.setData(  
+          {  
+            showModalStatus: false  
+          }  
+        );  
+      }  
+    }.bind(this), 200)  
+
+    // 显示  
+    if (currentStatu == "open") {  
+      this.setData(  
+        {  
+          showModalStatus: true  
+        }  
+      );  
+    }  
+  },  
+
    /**上传数据至数据库 */
-   saveNewTodo: function(){
+   saveNewTodo: function(e){
      let self = this,
      studytype=self.data.studytype,
      studyitem=self.data.studyitem,
@@ -66,61 +121,8 @@ Page({
          minute: self.data.target_minute
        },
     })
-   },
+    this.util("close")
+   }
 
-   /**弹窗动画**/
-    powerDrawer: function (e) {  
-      var currentStatu = e.currentTarget.dataset.statu;  
-      this.util(currentStatu)  
-    },  
-
-    util: function(currentStatu){  
-      /* 动画部分 */  
-      // 第1步：创建动画实例   
-      var animation = wx.createAnimation({  
-        duration: 200,  //动画时长  
-        timingFunction: "linear", //线性  
-        delay: 0  //0则不延迟  
-      }); 
-
-      // 第2步：这个动画实例赋给当前的动画实例  
-      this.animation = animation;
-
-      // 第3步：执行第一组动画  
-      animation.opacity(0).rotateX(-100).step();  
-    
-      // 第4步：导出动画对象赋给数据对象储存  
-      this.setData({  
-        animationData: animation.export()  
-      })  
-        
-      // 第5步：设置定时器到指定时候后，执行第二组动画  
-      setTimeout(function () {  
-        // 执行第二组动画  
-        animation.opacity(1).rotateX(0).step();  
-        // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
-        this.setData({  
-          animationData: animation  
-        })  
-          
-        //关闭  
-        if (currentStatu == "close") {  
-          this.setData(  
-            {  
-              showModalStatus: false  
-            }  
-          );  
-        }  
-      }.bind(this), 200)  
-
-      // 显示  
-      if (currentStatu == "open") {  
-        this.setData(  
-          {  
-            showModalStatus: true  
-          }  
-        );  
-      }  
-    }  
 
   })  
