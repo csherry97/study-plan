09ca8e0b7a109a832847b7a4156356602abafec1
csherry97 <zycai0227@gmail.com>
2020-06-15 22:29:08 +0800

    请假弹窗+页面时间

--- miniprogram/pages/todo/todo.js
+++ miniprogram/pages/todo/todo.js
@@ -4,24 +4,53 @@ Page({
 
     data: {
       array:[],
-      showModalStatus: false,
+      showModalStatus: "na",
       select: false,
       studytype: "学习",
       studyitem: "",
       target_hour: 0,
       target_minute:0,
-      status: "notfinished"
+      status: "notchecked",
+      reason: "",
+      /**top data */
+      finishedItem: 0,
+      finishedTime: 0,
+      unfinishedItem: 0,
+      unfinishedTime: 0
     }, 
 
     onShow: function(){
       let that = this;
+      let _array = [];
+      let unTime=0;
+      let unNum=0;
+      let fTime=0;
+      let fNum=0;
       wx.cloud.init();
       wx.cloud.database().collection("data").get({
         success(res){
           console.log("请求成功",res);
+          _array = res.data;
+          _array.forEach((value, index) => {
+            value['isShow'] = false;
+          });
+
+          for(var i=0; i<_array.length; i++){
+            if(_array[i].status == "finished"){
+              fTime += _array[i].target_time;
+              fNum += 1;
+            } else {
+              unTime += _array[i].target_time;
+              unNum += 1;
+            }
+          }
           that.setData({
-            array:res.data
-          })
+            array:_array,
+            unfinishedTime: unTime,
+            unfinishedItem: unNum,
+            finishedTime: fTime,
+            finishedItem: fNum,
+          });
         },
         fail(res){
           console.log("请求失败",res);
@@ -29,21 +58,23 @@ Page({
       })
     },
 
+
     /**下拉菜单**/
     bindShowMsg() {
-        this.setData({
-            select:!this.data.select
-        })
+      this.setData({
+          select:!this.data.select
+      })
    },
 
    mySelect(e) {
-       var name = e.currentTarget.dataset.name
-       this.setData({
-            studytype: name,
-            select: false
-       })
+      var name = e.currentTarget.dataset.name
+      this.setData({
+        studytype: name,
+        select: false
+      })
    },
-   /**input*/
+
+   /**添加待办表单input*/
    getItemValue: function(e){
      this.setData({studyitem: e.detail.value});
    },
@@ -56,13 +87,35 @@ Page({
     this.setData({target_minute: e.detail.value});
    },
 
+   getReasonValue: function(e){
+    this.setData({reason: e.detail.value});
+   },
+
    /**弹窗动画**/
    powerDrawer: function (e) {  
     var currentStatu = e.currentTarget.dataset.statu;  
-    this.util(currentStatu)  
-  },  
+    var changeStatus = "addTodo";
 
-  util: function(currentStatu){  
+    this.util(currentStatu,changeStatus);  
+  }, 
+  
+  absenceDrawer: function (e){
+    var changeStatus = "addAbsence";
+    var itemID= e.currentTarget.dataset.currentId;
+    var _array=this.data.array;
+    for(var i = 0; i < this.data.array.length; i++){
+      _array[i].isShow=false;
+      if (_array[i]._id == itemID){
+        _array[i].isShow=true;
+        this.util("open",changeStatus);  
+      } 
+    };
+    this.setData({  
+      array: _array 
+    })  
+  },
+
+  util: function(currentStatu, changeStatus){  
     /* 动画部分 */  
     // 第1步：创建动画实例   
     var animation = wx.createAnimation({  
@@ -95,7 +148,7 @@ Page({
       if (currentStatu == "close") {  
         this.setData(  
           {  
-            showModalStatus: false  
+            showModalStatus: "na"  
           }  
         );  
       }  
@@ -105,13 +158,13 @@ Page({
     if (currentStatu == "open") {  
       this.setData(  
         {  
-          showModalStatus: true  
+          showModalStatus: changeStatus 
         }  
       );  
     }  
   },  
-
-   /**上传数据至数据库 */
+  
+   /**上传表单数据至数据库 */
    saveNewTodo: function(e){
      let self = this,
      studytype=self.data.studytype,
@@ -139,11 +192,28 @@ Page({
          type:self.data.studytype,
          item:self.data.studyitem,
          target_time: convertHour,
-         status:self.data.status
+         status:self.data.status,
+         reason:self.data.reason
        },
     })
     this.util("close"),
-    this.onShow(),
-    this.onReady()
-   }
-  })  
+    this.onShow()
+   },
+  
+  updateAbsence: function(e){
+    const db=wx.cloud.database();
+    var goodId = e.currentTarget.dataset.currentId;
+    //获取goodId的方法同删除操作
+    db.collection("data").doc(goodId).update({
+    //只修改goodNum属性
+      data: {
+        status: "absence",
+        reason: this.data.reason
+      },
+    })
+    this.setData
+    this.util("close"),
+    this.onShow()
+  },
+
+})  
